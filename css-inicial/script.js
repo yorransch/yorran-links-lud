2b169839ba9ddb4e6481947cbfbc83c0491043f3
Yorran <90165535+yorransch@users.noreply.github.com>
2023-06-21 17:44:45 +0200

    inicial
    links
    
    html
    css
    js

---
+++ css-inicial/script.js
@@ -0,0 +1,13 @@
+function tooglemod(){
+    const html =document.documentElement
+
+    //if (html.classList.contains("ligth")) {
+      //  html.classList.remove("ligth") 
+
+    //}else{
+        //html.classList.add("ligth")
+   // }
+
+    html.classList.toggle("ligth")
+
+}
